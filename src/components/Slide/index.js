281cab957489f97bbcb0c2888870c4dc9af9c215
99roberto <diego.bruning@nl.com.br>
2022-09-06 09:17:24 -0300

    slids

--- src/components/Slide/index.js
+++ src/components/Slide/index.js
@@ -4,23 +4,23 @@ function Slide() {
   return (
     <Carousel>
       <Carousel.Item className="slide">
-        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="/flight-4516478.jpg" alt="First slide" />
+        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="https://99roberto.github.io/fly_well/flight-4516478.jpg" alt="First slide" />
       </Carousel.Item>
       <Carousel.Item className="slide">
-        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="/baggage-hall-775540.jpg" alt="Second slide" />
+        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="https://99roberto.github.io/fly_well/baggage-hall-775540.jpg" alt="Second slide" />
       </Carousel.Item>
 
       <Carousel.Item className="slide">
-        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="/architecture-3337986.jpg" alt="Second slide" />
+        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="https://99roberto.github.io/fly_well/architecture-3337986.jpg" alt="Second slide" />
       </Carousel.Item>
       <Carousel.Item className="slide">
-        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="/airplane-5216583_1920.png" alt="Second slide" />
+        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="https://99roberto.github.io/fly_well/airplane-5216583_1920.png" alt="Second slide" />
       </Carousel.Item>
       <Carousel.Item className="slide">
-        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="/airplane-4974678.jpg" alt="Second slide" />
+        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="https://99roberto.github.io/fly_well/airplane-4974678.jpg" alt="Second slide" />
       </Carousel.Item>
       <Carousel.Item className="slide">
-        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="/aircraft-take-off-123028.jpg" alt="Second slide" />
+        <img style={{ height: 600, maxHeight: 600, width: "100%" }} src="https://99roberto.github.io/fly_well/aircraft-take-off-123028.jpg" alt="Second slide" />
       </Carousel.Item>
     </Carousel>
   );
