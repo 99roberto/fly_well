20481d8c62cf335ef77ce824b302ee48210727ad
99ana <ana.agencia99@yahoo.com>
2022-12-14 14:07:16 -0300

    akuste email

--- src/constantes.js
+++ src/constantes.js
@@ -2,4 +2,4 @@ export const AppName = "Voe Bem";
 export const SubTitulo = "Voe Bem";
 export const AppUrlHome = "http://voebem.app.br/"
 export const AppDomine = "voebem.app.br"
-export const AdmEmail = "flywell@gmail.com"
+export const AdmEmail = "voebemapp@gmail.com"
